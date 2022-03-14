e87451d39a575becb1669b0bd298e8aaaff1ad20
HAWNBINYOO <dngh0825@naver.com>
2022-03-14 21:14:15 +0900

    3/14/21

---
+++ todo_list/src/component/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import * as S from './Style';
+
+const Header = () => {
+  return (
+    <>
+      <S.Header>
+        <h1>TodoApp</h1>
+      </S.Header>
+    </>
+  );
+};
+
+export default Header;
