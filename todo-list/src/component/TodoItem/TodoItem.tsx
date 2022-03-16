746fa14b6e39634533ca4e589a488d10ed3aeb28
HAWNBINYOO <dngh0825@naver.com>
2022-03-16 20:44:24 +0900

    3/16/20

--- todo-list/src/component/TodoItem/TodoItem.tsx
+++ todo-list/src/component/TodoItem/TodoItem.tsx
@@ -2,13 +2,11 @@ import React from "react";
 import * as S from "./Style";
 
 const TodoItem = ({ todo, onRemove }: { todo: any; onRemove: any }) => {
-  const { id, text } = todo;
-
   return (
     <>
       <S.TodoItem>
-        {text}
-        <S.DelectButton onClick={() => onRemove(id)}>삭제</S.DelectButton>
+        {todo.text}
+        <S.DelectButton onClick={() => onRemove(todo.id)}>삭제</S.DelectButton>
       </S.TodoItem>
     </>
   );
