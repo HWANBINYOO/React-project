0bc25f5262fc0f41dc97d6e4d298ea773f9abd39
HAWNBINYOO <dngh0825@naver.com>
2022-04-21 10:29:48 +0900

    4/21/10

--- vlog/public/main.js
+++ vlog/public/main.js
@@ -3,9 +3,6 @@ const path = require("path");
 const url = require("url");
 
 function createWindow() {
-  /*
-   * 넓이 1920에 높이 1080의 FHD 풀스크린 앱을 실행시킵니다.
-   * */
   const win = new BrowserWindow({
     width: 765,
     height: 762,
@@ -16,7 +13,6 @@ function createWindow() {
    * 만일 URL을 따로 지정하지 않을경우 (프로덕션빌드) React 앱이
    * 빌드되는 build 폴더의 index.html 파일을 로드합니다.
    * */
-
   const startUrl =
     process.env.ELECTRON_START_URL ||
     url.format({
@@ -24,9 +20,6 @@ function createWindow() {
       protocol: "file:",
       slashes: true,
     });
-  /*
-   * startUrl에 배정되는 url을 맨 위에서 생성한 BrowserWindow에서 실행시킵니다.
-   * */
   win.loadURL(startUrl);
 }
 
