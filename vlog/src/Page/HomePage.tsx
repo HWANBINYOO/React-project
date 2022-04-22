2bc2d5f36ba21aacca9ed255527a7264a7883265
HAWNBINYOO <dngh0825@naver.com>
2022-04-22 15:11:03 +0900

    4/22/15

--- vlog/src/Page/HomePage.tsx
+++ vlog/src/Page/HomePage.tsx
@@ -6,11 +6,13 @@ import isLogin from "../Libs/isLogin";
 
 export default function HomePage() {
   const navigate = useNavigate();
+  isLogin() ? navigate("/about") : navigate("/");
+
   return (
     <>
       <Title />
       {/* 만약 로그인이 됬다면 바로 about 페이지로 가게하기 */}
-      {!isLogin() ? navigate("/about") : navigate("/")}
+
       <Home />
     </>
   );
