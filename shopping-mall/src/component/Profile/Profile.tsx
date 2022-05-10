012eb6df195eea58ba048e9b06e387dd585bcecb
HAWNBINYOO <dngh0825@naver.com>
2022-05-10 19:28:27 +0900

    profile 구조

--- shopping-mall/src/component/Profile/Profile.tsx
+++ shopping-mall/src/component/Profile/Profile.tsx
@@ -3,7 +3,68 @@ import * as S from "./Styled";
 const Profile = () => {
   return (
     <>
-      <S.Profile></S.Profile>
+      <S.Postionner>
+        <S.MyImformation>
+          <S.ProfileImg />
+          <S.MyPrivacy>
+            <S.MyPrivacyTop>
+              <S.Name>정재훈</S.Name>
+              <S.EditBtn></S.EditBtn>
+            </S.MyPrivacyTop>
+            <S.Money>1000,000,000</S.Money>
+            <S.Email>woocom424@gmail.com</S.Email>
+          </S.MyPrivacy>
+        </S.MyImformation>
+        <S.MyLists>
+          <S.MyList>
+            <S.ListTitle>판매목록</S.ListTitle>
+            <S.Mysale>
+              <S.Sale>
+                <S.SaleImg></S.SaleImg>
+                <S.SaleImformation>
+                  <S.ImTop>
+                    <S.SaleName>로만손 남성 시계</S.SaleName>
+                    <S.DelectBtn>x</S.DelectBtn>
+                  </S.ImTop>
+                  <S.EmdDate>경매 만료일:2022년 5월 5일</S.EmdDate>
+                </S.SaleImformation>
+              </S.Sale>
+            </S.Mysale>
+          </S.MyList>
+
+          <S.MyList>
+            <S.ListTitle>경매목록</S.ListTitle>
+            <S.Mysale>
+              <S.Sale>
+                <S.SaleImg></S.SaleImg>
+                <S.SaleImformation>
+                  <S.ImTop>
+                    <S.SaleName>로만손 남성 시계</S.SaleName>
+                    <S.DelectBtn>x</S.DelectBtn>
+                  </S.ImTop>
+                  <S.ActionMoney>참여 금액:580,000</S.ActionMoney>
+                  <S.EmdDate>경매 만료일:2022년 5월 5일</S.EmdDate>
+                </S.SaleImformation>
+              </S.Sale>
+            </S.Mysale>
+          </S.MyList>
+
+          <S.MyList>
+            <S.Mysale>
+              <S.Sale>
+                <S.SaleImg></S.SaleImg>
+                <S.SaleImformation>
+                  <S.ImTop>
+                    <S.SaleName>로만손 남성 시계</S.SaleName>
+                    <S.DelectBtn>x</S.DelectBtn>
+                  </S.ImTop>
+                  <S.EmdDate>경매 만료일:2022년 5월 5일</S.EmdDate>
+                </S.SaleImformation>
+              </S.Sale>
+            </S.Mysale>
+          </S.MyList>
+        </S.MyLists>
+      </S.Postionner>
     </>
   );
 };
--- shopping-mall/src/component/Profile/Styled.ts
+++ shopping-mall/src/component/Profile/Styled.ts
@@ -1,3 +1,28 @@
 import styled from "styled-components";
 
-export const Profile = styled.div``;
+export const Postionner = styled.div`
+  height: 80vh;
+  width: 100%;
+  justify-content: center;
+`;
+export const MyImformation = styled.div``;
+export const ProfileImg = styled.div``;
+export const MyPrivacy = styled.div``;
+export const MyPrivacyTop = styled.div``;
+export const Name = styled.div``;
+export const EditBtn = styled.div``;
+export const Money = styled.div``;
+export const Email = styled.div``;
+export const MyLists = styled.div``;
+export const MyList = styled.div``;
+export const ListTitle = styled.div``;
+export const Mysale = styled.div``;
+export const MyAaction = styled.div``;
+export const Sale = styled.div``;
+export const SaleImg = styled.div``;
+export const SaleImformation = styled.div``;
+export const ImTop = styled.div``;
+export const ActionMoney = styled.div``;
+export const SaleName = styled.div``;
+export const DelectBtn = styled.div``;
+export const EmdDate = styled.div``;
