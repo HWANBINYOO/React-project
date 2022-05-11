54687cb4185c29b67891987b8ebbcc308e75b96b
HAWNBINYOO <dngh0825@naver.com>
2022-05-11 10:33:21 +0900

    Sign in axois

--- shopping-mall/src/component/SignIn/SignIn.tsx
+++ shopping-mall/src/component/SignIn/SignIn.tsx
@@ -1,18 +1,44 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import * as S from "./Styled";
 
 const SignIn = () => {
+  const [Email, setEmail] = useState<string>("");
+  const [PassWord, setPassWord] = useState<string>("");
+  const navigate = useNavigate();
+
+  const onLogin = async () => {
+    console.log(Email, PassWord);
+    const { data } = await axios.post("/login", {
+      email: Email,
+      password: PassWord,
+    });
+
+    console.log(data);
+    localStorage.setItem("Blog_accessToken", data.accessToken);
+    localStorage.setItem("Blog_refreshToken", data.refreshToken);
+
+    navigate("/about");
+  };
+  const onChangeEmail = (e: any) => {
+    setEmail(e.currentTarget.value);
+  };
+  const onChangePassWord = (e: any) => {
+    setPassWord(e.currentTarget.value);
+  };
   return (
     <>
       <S.Positioner>
         <S.Title>Search</S.Title>
         <S.SigninBox>
           <S.inputBox>
-            <input placeholder="email" />
+            <input placeholder="email" onChange={onChangeEmail} />
           </S.inputBox>
           <S.inputBox>
-            <input placeholder="password" />
+            <input placeholder="password" onChange={onChangePassWord} />
           </S.inputBox>
-          <S.Btn>Sign in</S.Btn>
+          <S.Btn onClick={() => onLogin()}>Sign in</S.Btn>
         </S.SigninBox>
       </S.Positioner>
     </>
