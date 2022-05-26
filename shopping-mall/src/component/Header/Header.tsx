d9dbf793c37dd5dad339ee1f6d1b687b4165e90f
HAWNBINYOO <dngh0825@naver.com>
2022-05-26 13:48:10 +0900

    header 퍼블리싱

--- shopping-mall/src/component/Header/Header.tsx
+++ shopping-mall/src/component/Header/Header.tsx
@@ -1,23 +1,26 @@
+import { Link } from "react-router-dom";
 import * as S from "./Styled";
 
 const Header = () => {
   return (
     <>
       <S.Header>
-        <S.Sign>
-          <S.SignInUp>Sign in</S.SignInUp>
-          <S.SignInUp>Sign up</S.SignInUp>
-        </S.Sign>
         <S.HeaderBottom>
-          <S.Mainlogo>Search</S.Mainlogo>
-          <S.InputBox>
-            <S.Input />
-            <S.SearchImg src={"/img/search.png"} />
-          </S.InputBox>
-          <S.HeaderLogos>
-            <S.ProfileImg src={"/img/profile.png"} />
-            <S.ProfileImg src={"/img/new.png"} />
-          </S.HeaderLogos>
+          <S.CircleImg src={"/img/Ellipse.png"} />
+          <S.Mainlogo src={"/img/Wear.png"} />
+          <S.HeaderRight>
+            <S.HeaderLogos>
+              <S.ProfileImg src={"/img/menu.png"} />
+              <S.ProfileImg src={"/img/searchIcon.png"} />
+              <S.ProfileImg src={"/img/basket.png"} />
+            </S.HeaderLogos>
+            <Link
+              to="/signIn"
+              style={{ textDecoration: "none", color: "black" }}
+            >
+              <S.LoginButton>로그인/가입</S.LoginButton>
+            </Link>
+          </S.HeaderRight>
         </S.HeaderBottom>
       </S.Header>
     </>
--- shopping-mall/src/component/Header/Styled.ts
+++ shopping-mall/src/component/Header/Styled.ts
@@ -2,79 +2,55 @@ import styled from "styled-components";
 
 export const Header = styled.div`
   width: 100%;
-  height: 15vh;
+  height: 10vh;
   display: flex;
-  flex-direction: column;
+  align-items: center;
   gap: 5px;
+  background-color: white;
 `;
-
-export const Sign = styled.div`
-  font-family: "Inika", serif;
-  height: 3vh;
-  display: flex;
-  justify-content: flex-end;
-  align-items: flex-start;
-  gap: 20px;
-  padding-right: 3%;
-  color: #755139;
-`;
-
-export const SignInUp = styled.div`
-  font-size: 1.1rem;
-`;
-
 export const HeaderBottom = styled.div`
-  height: 12vh;
-  width: 100%;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  background-color: #024959;
-`;
-
-export const Mainlogo = styled.div`
-  font-family: "Inika", serif;
-  font-size: 2.5rem;
-  font-weight: 100;
-  color: #f2edd7;
-`;
-export const InputBox = styled.h1`
-  width: 50%;
-  height: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 10px;
-`;
-
-export const Input = styled.input`
-  width: 80%;
-  height: 80%;
-  border-radius: 10px;
-  background-color: #f2edd7;
-  text-align: left;
-  padding-left: 20px;
-  outline: none;
-  border: none;
-  font-size: 1.5rem;
-  color: #755139;
-
-  &:focus {
-    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-  }
-`;
-
-export const SearchImg = styled.img`
-  height: 60%;
-`;
-export const HeaderLogos = styled.div`
+  height: 10vh;
+  width: 95%;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  width: 6%;
+  margin: 0 auto;
+`;
+export const HeaderRight = styled.div`
+  width: 400px;
+  height: 80%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const LoginButton = styled.button`
+  font-family: "EarlyFontDiary";
+  height: 3.5vh;
+  width: 150px;
+  border: none;
+  border-radius: 10px;
+  background-color: black;
+  color: white;
+  font-size: 0.8rem;
+`;
+
+export const CircleImg = styled.img`
+  height: 40%;
+`;
+export const Mainlogo = styled.img`
+  padding-left: 12vw;
+  height: 30%;
+`;
+
+export const HeaderLogos = styled.div`
+  width: 200px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const ProfileImg = styled.img`
-  width: 30px;
+  width: 25px;
   height: 60%;
 `;
