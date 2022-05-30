7c96c7e59a5677af229778a306670bedfaef0f93
HAWNBINYOO <dngh0825@naver.com>
2022-05-30 23:48:20 +0900

    상세페이지

--- shopping-mall/src/component/KindIn/KindIn.tsx
+++ shopping-mall/src/component/KindIn/KindIn.tsx
@@ -1,7 +1,30 @@
 import * as S from "./Styled";
 
 const KindIn = () => {
-  return <S.KindInWapper></S.KindInWapper>;
+  return (
+
+    <S.KindInWapper>
+        <S.Header>
+            <S.MainLogo></S.MainLogo>
+            <S.InputBox>
+                <S.SearchInput />
+                <S.SearchIcon></S.SearchIcon>
+            </S.InputBox>
+        </S.Header> 
+        <S.Contents>
+            <S.LeftContent></S.LeftContent>
+            <S.KindInImg></S.KindInImg>
+            <S.KindRight>
+                <S.KindDecs>
+                    <S.KindTitle></S.KindTitle>
+                    <S.KindSize></S.KindSize>
+                </S.KindDecs>
+                <S.Kind></S.Kind>
+            </S.KindDecs>
+        </S.Contents>
+
+  </S.KindInWapper>;
+      )
 };
 
 export default KindIn;
