7d083f022d95596a375d154a150155aec28b8143
HAWNBINYOO <dngh0825@naver.com>
2022-06-29 11:47:04 +0900

    domain

--- vlog/src/Libs/CustomAxois.ts
+++ vlog/src/Libs/CustomAxois.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from "axios";
 import { getRefresh } from "./getRefresh";
 
 export const customAxios: AxiosInstance = axios.create({
-  baseURL: "http://52.79.227.100/",
+  baseURL: "https://dev-log.kr/",
 });
 
 customAxios.interceptors.request.use(getRefresh);
