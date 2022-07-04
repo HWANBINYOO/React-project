c3feab33189861fe65eb15db992879b342844b5b
HAWNBINYOO <dngh0825@naver.com>
2022-07-04 11:41:34 +0900

    url

--- vlog/src/Libs/CustomAxois.ts
+++ vlog/src/Libs/CustomAxois.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 import { getRefresh } from "./getRefresh";
 
 export const customAxios: AxiosInstance = axios.create({
-  baseURL: "http://10.53.68.207:8080/",
+  baseURL: "https://server.dev-log.kr/",
 });
 
 customAxios.interceptors.request.use(getRefresh);
